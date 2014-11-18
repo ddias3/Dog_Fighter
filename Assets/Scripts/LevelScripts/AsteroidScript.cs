3bd3707a89be23fb464d35b0e59a6c3c94029da5
Jacob Hudlow <jhudlow3@gatech.edu>
2014-11-17 22:28:04 -0500

    Asteroid Movement Stuff
    The asteroids in the scene move and rotate, need to add this to prefabs
    and randomize the speed some.

---
+++ Assets/Scripts/LevelScripts/AsteroidScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidScript : MonoBehaviour {
+	
+	Vector3 rotate;
+	
+	// Use this for initialization
+	void Start () {
+		Vector3 force = new Vector3(Random.value-.5f,Random.value-.5f,Random.value-.5f);
+		force.Normalize();
+		rigidbody.AddForce(force*10000000);
+		rotate = new Vector3(Random.value-.5f,Random.value-.5f,Random.value-.5f);
+		rotate.Normalize();
+		rotate = rotate/10f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles+rotate);
+	}
+	
+	void OnCollisionEnter(Collision collision){
+		rotate *= -1f;
+	}
+}
