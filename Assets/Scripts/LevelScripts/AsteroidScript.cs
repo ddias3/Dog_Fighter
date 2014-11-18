c15c386eb2e5570078c7c8c24399816987e5afb0
Jacob Hudlow <jhudlow3@gatech.edu>
2014-11-18 17:27:10 -0500

    Randomized asteroid movement and rotation speeds

--- Assets/Scripts/LevelScripts/AsteroidScript.cs
+++ Assets/Scripts/LevelScripts/AsteroidScript.cs
@@ -9,10 +9,10 @@ public class AsteroidScript : MonoBehaviour {
 	void Start () {
 		Vector3 force = new Vector3(Random.value-.5f,Random.value-.5f,Random.value-.5f);
 		force.Normalize();
-		rigidbody.AddForce(force*10000000);
+		rigidbody.AddForce(force*50000000/((Random.value*9f)+1));
 		rotate = new Vector3(Random.value-.5f,Random.value-.5f,Random.value-.5f);
 		rotate.Normalize();
-		rotate = rotate/5f;
+		rotate = rotate/(2.5f*((Random.value*3f)+1f));
 		rigidbody.angularVelocity = rotate;
 	}
 }
