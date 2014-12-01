2db0055b6dab73652197f512655e4e812f8525cf
Jacob Hudlow <jhudlow3@gatech.edu>
2014-12-01 16:25:02 -0500

    added unfinished scripts for missiles, laser, and flares

---
+++ Assets/Scripts/ShipScripts/FlareScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlareScript : MonoBehaviour {
+
+	public float coolDown=7f;
+	private float lastFired;
+	
+	void Start () {
+		lastFired = -999f;
+	}
+	
+	float Fire() {
+		if(Time.time-lastFired<coolDown){
+			lastFired = Time.time;
+			//also actually fire
+			return lastFired;
+		}
+	}
+}
---
+++ Assets/Scripts/ShipScripts/LaserScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserScript : MonoBehaviour {
+
+	public float coolDown=.5f;
+	private float lastFired;
+
+	void Start () {
+		lastFired = -999f;
+	}
+
+	float Fire() {
+		if(Time.time-lastFired<coolDown){
+			lastFired = Time.time;
+			//also actually fire
+			return lastFired;
+		}
+	}
+}
